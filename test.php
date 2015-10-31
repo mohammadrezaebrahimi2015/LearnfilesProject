3543ada53afad61e059964e6dab2361ca0e25305
mohammadrezaebrahimi2015 <melikaebrahimi2015@gmail.com>
2015-10-31 14:08:09 +0330

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,3 @@
-<html>
-<title>Leanfiles Github</title>
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<script src="script/script.js"></script>
-<body>
-<span id="box">لرن فایلز</span>
-<br>
-<a href="http://learnfiles.com">Learnfiles</a>
-</body>
-</html>
+<?php
+echo 'learnfiles.com';
+?>
