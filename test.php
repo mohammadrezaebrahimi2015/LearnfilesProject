8dd35623f8987228cb66e7863c08a16ac2652ccf
mohammadrezaebrahimi2015 <melikaebrahimi2015@gmail.com>
2015-10-31 14:02:46 +0330

    PHP File

---
+++ test.php
@@ -0,0 +1,10 @@
+<html>
+<title>Leanfiles Github</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="script/script.js"></script>
+<body>
+<span id="box">لرن فایلز</span>
+<br>
+<a href="http://learnfiles.com">Learnfiles</a>
+</body>
+</html>
